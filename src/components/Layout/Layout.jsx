0f5f13679e4c67ba529eba635cb3e7c7a84198ff
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-11-15 23:37:57 -0500

    new layout nav top fixed sidebar left fixed top nav 100% of screen

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,11 +5,9 @@ import Sidebar from "../Sidebar/Sidebar";
 
 const Layout = (props) => (
   <div className="layout">
-    
-    <Sidebar user={props.user} />
-
+    <Nav user={props.user} />
     <div className="main-content">
-      <Nav user={props.user} />
+      <Sidebar user={props.user} />
       <div className="layout-children">{props.children}</div>
       <Footer />
     </div>
