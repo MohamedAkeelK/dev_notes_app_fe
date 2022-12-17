1b1c65268200088bc78aa13101e694be1fdb8562
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-12-17 04:21:07 -0500

    updated styles

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -10,7 +10,7 @@ const Layout = (props) => (
     <div className="after-nav-elems">
       <Sidebar user={props.user} />
       <div className="children">{props.children}</div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   </div>
 );
