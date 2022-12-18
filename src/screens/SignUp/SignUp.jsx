47eacbbc59f710b7e5d61983ca682f8e17312851
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-12-18 02:09:46 -0500

    sign up and sign out pages styling

--- src/screens/SignUp/SignUp.jsx
+++ src/screens/SignUp/SignUp.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./SignUp.css";
 import { signUp } from "../../services/users";
 import { useNavigate } from "react-router-dom";
+import Nav from "../../components/Nav/Nav.jsx";
 
 const SignUp = (props) => {
   const navigate = useNavigate();
@@ -50,55 +51,63 @@ const SignUp = (props) => {
         </button>
       );
     } else {
-      return <button type="submit">Sign Up</button>;
+      return (
+        <button type="submit" className="submit-button">
+          Sign Up
+        </button>
+      );
     }
   };
 
   const { username, email, password_digest, passwordConfirmation } = form;
 
   return (
-    <div className="form-container">
-      <h3>Sign Up</h3>
-      <form onSubmit={onSignUp}>
-        <label>Username</label>
-        <input
-          required
-          type="text"
-          name="username"
-          value={username}
-          placeholder="Enter username"
-          onChange={handleChange}
-        />
-        <label>Email address</label>
-        <input
-          required
-          type="email"
-          name="email"
-          value={email}
-          placeholder="Enter email"
-          onChange={handleChange}
-        />
-        <label>Password</label>
-        <input
-          required
-          name="password_digest"
-          value={password_digest}
-          type="password"
-          placeholder="Password"
-          onChange={handleChange}
-        />
-        <label>Password Confirmation</label>
-        <input
-          required
-          name="passwordConfirmation"
-          value={passwordConfirmation}
-          type="password"
-          placeholder="Confirm Password"
-          onChange={handleChange}
-        />
-        {renderError()}
-      </form>
-    </div>
+    <>
+      <Nav />
+      <div className="signup-form-container">
+        <form onSubmit={onSignUp} className="signup-form">
+          <h1>Welcome to DevNotes</h1>
+          <h2>Sign up to Start Posting!</h2>
+          <label>Username</label>
+          <input
+            required
+            type="text"
+            name="username"
+            value={username}
+            placeholder="Enter username"
+            onChange={handleChange}
+          />
+          <label>Email address</label>
+          <input
+            required
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Enter email"
+            onChange={handleChange}
+          />
+          <label>Password</label>
+          <input
+            required
+            name="password_digest"
+            value={password_digest}
+            type="password"
+            placeholder="Password"
+            onChange={handleChange}
+          />
+          <label>Password Confirmation</label>
+          <input
+            required
+            name="passwordConfirmation"
+            value={passwordConfirmation}
+            type="password"
+            placeholder="Confirm Password"
+            onChange={handleChange}
+          />
+          {renderError()}
+        </form>
+      </div>
+    </>
   );
 };
 
